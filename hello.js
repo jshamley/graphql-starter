e5152426b4fdf798f407f51e786228335f506f37
Jeff Shamley <jshamley@comcast.net>
2017-12-19 11:16:16 -0700

    hello world

---
+++ hello.js
@@ -0,0 +1,13 @@
+const { graphql, buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello World!' };
+
+graphql(schema, '{ hello }', root).then(response => {
+  console.log(response);
+});
